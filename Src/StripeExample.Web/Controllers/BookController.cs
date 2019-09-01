aa070515b7aec60f014ba66ffeffd96f37b184f1
S Lokesh Swamy <lokesh.selvaraj@hotmail.com>
2019-09-01 21:58:08 +0530

    Added ViewModels for Book Controller, refactored POST action + Included Keys from StripeAPI + Added validation logic for Custom webpage + minor UI Cleanup

--- Src/StripeExample.Web/Controllers/BookController.cs
+++ Src/StripeExample.Web/Controllers/BookController.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using StripeExample.Web.Models.Book;
+using System.Configuration;
+using System.Diagnostics;
 using System.Web.Mvc;
 
 namespace StripeExample.Web.Controllers
@@ -7,14 +9,40 @@ namespace StripeExample.Web.Controllers
     {
         // embedded form
         public ActionResult Index()
-        {            
+        {
+            string stripePublishableKey = ConfigurationManager.AppSettings["stripePublishableKey"];
+            var model = new IndexViewModel() { StripePublishableKey = stripePublishableKey };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Charge(ChargeViewModel chargeViewModel)
+        {
+            Debug.WriteLine(chargeViewModel.StripeEmail);
+            Debug.WriteLine(chargeViewModel.StripeToken);
+            return RedirectToAction("Confirmation");
+        }
+
+        public ActionResult Confirmation()
+        {
             return View();
         }
 
         //custom form
         public ActionResult Custom()
         {
-            return View();
+            string stripePublishableKey = ConfigurationManager.AppSettings["stripePublishableKey"];
+            var model = new CustomViewModel() { StripePublishableKey = stripePublishableKey };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Custom(CustomViewModel customViewModel)
+        {
+            System.Diagnostics.Debug.WriteLine(customViewModel);
+            return RedirectToAction("Confirmation");
         }
     }
 }
---
+++ Src/StripeExample.Web/Models/BookViewModel.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * COM_001 - To resolve ambiguity between
+ * 
+ * ~/Models/ 
+ *         /ManageViewModels.IndexViewModel     and,
+ *         /Book.IndexViewModel 
+ */
+
+//namespace StripeExample.Web.Models -> COM_001
+
+namespace StripeExample.Web.Models.Book
+{
+    public class IndexViewModel
+    {
+        public string StripePublishableKey { get; set; }
+    }
+
+    public class ChargeViewModel
+    {
+        public string StripeToken { get; set; }
+        public string StripeEmail { get; set; }
+    }
+
+    public class CustomViewModel
+    {
+        public string StripePublishableKey { get; set; }
+        public string StripeToken { get; set; }
+        public string StripeEmail { get; set; }
+    }
+}
