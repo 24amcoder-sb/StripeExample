f4f42c96ec7b23aee9b1040c932c50afdc5796ee
S Lokesh Swamy <lokesh.selvaraj@hotmail.com>
2019-09-01 19:30:35 +0530

    Added UI for OneTime , Custom & Subscription webpages

---
+++ Src/StripeExample.Web/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+
+namespace StripeExample.Web.Controllers
+{
+    public class BookController : Controller
+    {
+        // embedded form
+        public ActionResult Index()
+        {            
+            return View();
+        }
+
+        //custom form
+        public ActionResult Custom()
+        {
+            return View();
+        }
+    }
+}
