2149d59393f6159bc5b16f961d04af920a44166c
S Lokesh Swamy <lokesh.selvaraj@hotmail.com>
2019-09-03 00:08:27 +0530

    Implemented Webhook logic for refund including email service + Added slowcheetah for config trasnformations

---
+++ Src/StripeExample.Web/Controllers/StripeWebhookController.cs
@@ -0,0 +1,49 @@
+﻿using Stripe;
+using System;
+using System.IO;
+using System.Net;
+using System.Web.Mvc;
+
+namespace StripeExample.Web.Controllers
+{
+    public class StripeWebhookController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index()
+        {
+            Stream request = Request.InputStream;
+            request.Seek(0, SeekOrigin.Begin);
+            string json = new StreamReader(request).ReadToEnd();
+
+            StripeEvent stripeEvent = null;
+
+            try
+            {
+                stripeEvent = StripeEventUtility.ParseEvent(json);
+            }
+            catch (Exception e)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, string.Format("Unable to parse incoming event. The following error occured: {0}", e.Message));
+            }
+
+            if(stripeEvent == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Incoming event empty");
+
+            var emailService = new Services.EmailService();
+
+
+            switch (stripeEvent.Type)
+            {
+                case StripeEvents.ChargeRefunded:
+                    var charge = Mapper<StripeCharge>.MapFromJson(stripeEvent.Data.Object.ToString());
+                    emailService.SendRefundEmail(charge);
+                    break;
+
+                default:
+                    break;
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ Src/StripeExample.Web/Services/EmailService.cs
@@ -0,0 +1,21 @@
+﻿using Stripe;
+using System.Net.Mail;
+
+namespace StripeExample.Web.Services
+{
+    public class EmailService
+    {
+        public void SendRefundEmail(StripeCharge stripeCharge)
+        {
+            var message = new MailMessage() { IsBodyHtml = true };
+            message.To.Add(new MailAddress("RECIPIENT@outlook.com"));
+            message.Subject = string.Format("refund requested on charge: {0}", stripeCharge.Id);
+            message.Body = string.Format("<p>{0}</p>", string.Format("A customer at this email address {0} was issued a refund on their purchase: '{1}'.  Please follow up to determine a reason.", stripeCharge.ReceiptEmail, stripeCharge.Description));
+
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Send(message);
+            }
+        }
+    }
+}
